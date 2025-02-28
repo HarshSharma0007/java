f6eb9a50848c9f5381d78c4c53abc8f57d0e3e2d
Harsh Sharma <153315733+HarshSharma0007@users.noreply.github.com>
2025-03-01 03:47:56 +0530

    printstatements

---
+++ basics/p001_printstatements.java
@@ -0,0 +1,21 @@
+public class p001_printstatements{
+
+    public static void main(String[] args){
+
+
+
+        // THIS IS MY FIRST JAVA PROGRAM 
+
+        /*
+         * THIS
+         * IS
+         * A
+         * MULTI
+         * LINE
+         * COMMENT
+         */
+        System.out.println("I like pizza!");
+        System.out.println("It's really good");
+        System.out.println("Buy me pizza.");
+    }
+}
