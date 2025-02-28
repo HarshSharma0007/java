a847a5c62e004a515b89fca96bae06785ff508d3
Harsh Sharma <153315733+HarshSharma0007@users.noreply.github.com>
2025-03-01 05:17:56 +0530

    operators completed

---
+++ basics/p007_operators.java
@@ -0,0 +1,35 @@
+public class p007_operators {
+    public static void main(String[] args) {
+        // Airthmatic Operator
+        int x = 10;
+        int y = 2;
+        int z;
+
+        // Airthmatic Operator
+
+        // z = x + y;
+        // z = x - y;
+        // z = x * y;
+        // z = x / y;
+        z = x % y;
+        System.out.println(z);
+
+        // Augumented Assignment Operators
+
+        // x+=y;
+        // x-=y;
+        // x%=y;
+        // x*=y;
+        // x/=y;
+        
+
+        // increment and decrement
+        // x++;
+        // --x;
+
+// ALWAYS REMEMBER ORDER OF OPERATIONS [P-E-M-D-A-S]
+        double result = 3 + 4 * (7-5)/2.0;
+        System.out.println(result);
+    }
+    
+}
