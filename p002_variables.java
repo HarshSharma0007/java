4d0954243fccecadc1168c2a4faaff6addf5af32
Harsh Sharma <153315733+HarshSharma0007@users.noreply.github.com>
2025-03-01 04:02:05 +0530

    variables completed

---
+++ p002_variables.java
@@ -0,0 +1,26 @@
+public class p002_variables {
+    
+    public static void main(String[] args) {
+        int age = 20;
+        System.out.println("My age is " + age + "years.");
+
+        double price = 19.99;
+        System.out.println("$" + price);
+
+        char grade = 'A';
+        System.out.println("your grad is " + grade + ".");
+
+        boolean isStudent = true;
+
+
+        if(isStudent){
+            System.out.println("You are a student.");
+
+        }
+        else {
+            System.out.println("You are not a student.");
+        }
+        String name = "Ramon";
+        System.out.println("My name is " + name + ".");
+    }
+}
