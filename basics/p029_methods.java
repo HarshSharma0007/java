789697fae7c07f8f2147a9557b2ea3fd8c33b21e
Harsh Sharma <153315733+HarshSharma0007@users.noreply.github.com>
2025-03-02 19:01:41 +0530

    methods

---
+++ basics/p029_methods.java
@@ -0,0 +1,13 @@
+public class p029_methods {
+    public static void main(String[] args) {
+
+        String name = "lucky";
+        int age = 25;
+        happyBirthday(name,age);
+        
+    }
+    static void happyBirthday(String name, int age){
+        System.out.println("happy birthday to you " + name+"\nYour age is "+ age);
+    }
+    
+}
