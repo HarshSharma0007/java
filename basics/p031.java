7555e3b4f945d36a7ac6f99fd698d349683ae338
Harsh Sharma <153315733+HarshSharma0007@users.noreply.github.com>
2025-03-04 00:20:02 +0530

    banking Program

--- basics/p031.java
+++ basics/p031.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 
 public class p031 {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
 
         double balance=10;
         boolean isRunning = true;
@@ -24,21 +24,43 @@ public class p031 {
 
         switch (choice) {
             case 1-> showBalance(balance);
-            case 2-> showBalance(balance);
-            case 3-> showBalance(balance);
+            case 2-> balance+=deposit();
+            case 3-> balance-=withdraw(balance);
             case 4-> isRunning=false;
             default-> System.out.println("invalid choice");
+            }
         }
-
-        }
-
-    
+        System.out.println("********************");
         scanner.close();
-        
     }
     static void showBalance(double balance){
         System.out.printf("%.2f",balance);
     }
+    static double deposit(){
+        double amount;
+        System.out.println("Enter an amount to be deposit: ");
+        amount = scanner.nextDouble();
+        if (amount<0) {
+            System.out.println("Amount can't be Negative");
+            return 0;
+        }
+        else{
+            return amount;
+        }
+    }
+    static double withdraw(double balance) {
+        double amount;
+        System.out.print("Enter an amount to be withdrawn: ");
+        amount = scanner.nextDouble();
 
-    
+        if (amount > balance) {
+            System.out.println("Insufficient balance.");
+            return 0;
+        } else if (amount < 0) {
+            System.out.println("Amount can't be negative.");
+            return 0;
+        } else {
+            return amount;
+        }
+    }
 }
